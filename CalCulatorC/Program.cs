f14a9a1f354b68c68ee500bc9bd1ef3de6b92cab
Shokhjakhon Rustamov <84969400+shkhrustamov@users.noreply.github.com>
2023-12-07 10:16:55 +0100

    commit

---
+++ CalCulatorC/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace CalCulatorC // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
